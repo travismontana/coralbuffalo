5ee08b3bcf2c87a77db4188a8c8c8138574fad5b
Bob Bomar <bob@bomar.us>
2020-04-03 23:59:26 -0500

    Terraform 0.11 and earlier required type constraints to be given in quotes, but that form is now deprecated and will be removed in a future version of Terraform. To silence this warning, remove the quotes around "string".
    (and 6 more similar warnings elsewhere)

--- aws/main/modules/cb_standardtags/standardtags.tf
+++ aws/main/modules/cb_standardtags/standardtags.tf
@@ -1,34 +1,34 @@
 variable "Organization" {
   description = "What organization is this for (Infrastructure, Analytics, Enterprise Apps)"
-  type = "string"
+  type = string
 }
 
 variable "Owner" {
   description = "What group is responsible for this."
-  type = "string"
+  type = string
 }
 
 variable "Application" {
   description = "What overall application is this for."
-  type = "string"
+  type = string
 }
 
 variable "Environment" {
   description = "Production, Development, Test"
-  type = "string"
+  type = string
 }
 
 variable "SnapshotEnabled" {
   description = "Should snapshotting be enabled."
-  type = "string"
+  type = string
 }
 
 variable "AppVersion" {
   description = "What version of the application is this for."
-  type = "string"
+  type = string
 }
 
 variable "TaggingVersionID" {
   description = "What version of tags is this using."
-  type = "string"
+  type = string
 }
