7aa39eabf866d4524dfbfc5cdbd922446cc6aef4
Bob Bomar <bob@bomar.us>
2020-04-03 23:34:24 -0500

    Added standard tags to aws.

---
+++ aws/main/modules/cb_standardtags/output.tf
@@ -0,0 +1,40 @@
+output "coralbuffalo_common_tags_output" {
+  value = "${map(
+    "Organization", "${var.Organization}",
+    "Owner", "${var.Owner}",
+    "Application", "${var.Application}",
+    "Environment", "${var.Environment}",
+    "SnapshotEnabled", "${var.SnapshotEnabled}",
+    "Version", "${var.Version}",
+    "TaggingVersionID", "${var.TaggingVersionID}"
+    )
+  }"
+}
+
+output "Organization" {
+  value = "${var.Organization}"
+}
+
+output "Owner" {
+  value = "${var.Owner}"
+}
+
+output "Application" {
+  value = "${var.Application}"
+}
+
+output "Environment" {
+  value = "${var.Environment}"
+}
+
+output "SnapshotEnabled" {
+  value = "${var.SnapshotEnabled}"
+}
+
+output "Version" {
+  value = "${var.Version}"
+}
+
+output "TaggingVersionID" {
+  value = "${var.TaggingVersionID}"
+}
