620f0c02c38d751a42cb16b3610a658852440c98
travismontana <33264753+travismontana@users.noreply.github.com>
2020-07-30 09:53:28 -0500

    Setting up storage account

--- azure/storage_account/main.tf
+++ azure/storage_account/main.tf
@@ -6,3 +6,21 @@ provider "azurerm" {
   tenant_id       = file("/home/bob/shell/.azure/tenant_id")
 }
 
+resource "azurerm_resource_group" "cbhomedirs_rg" {
+  name = "cbhomedirs_rg"
+  location = "South Central US"
+}
+
+resource "azurerm_storage_account" "cbhomedirs_sa" {
+  name = "cbhomedirs_sa"
+  resource_group_name = azurerm_resource_group.cbhomedirs_rg.name
+  location = azurerm_resource_group.cbhomedirs_rg.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+
+  tags = "${merge(module.cbtagging.coralbuffalo_common_tags_output)}"
+}
+
+module "cbtagging" {
+  source = "git::https://github.com/travismontana/coralbuffalo.git//shared/cbtag"
+}
